6b83a6c0a17a67349a69336964508ee40319bf44
hernan3396 <hernan3396@gmail.com>
2022-04-12 11:40:18 -0300

    added weak point to objectives

--- Assets/BladeMode/Scripts/BladeTime.cs
+++ Assets/BladeMode/Scripts/BladeTime.cs
@@ -3,19 +3,39 @@ using EzySlice;
 
 public class BladeTime : MonoBehaviour
 {
+    [SerializeField] private GameObject _cubePrefab;
+    [SerializeField] private Transform _cubeSpawner;
+    #region Slicing
+    [Header("Slicing")]
     [SerializeField] private LayerMask _sliceableObjects;
     [SerializeField] private Transform _cuttingPlane;
     [SerializeField] private Material _crossMaterial;
+    #endregion
 
     #region SlicePosition
+    [Header("Slice Position")]
     [SerializeField] private Vector3 _initialPoint;
     [SerializeField] private Vector3 _finalPoint;
     [SerializeField] private Vector3 _cutPos;
     private Camera _cam;
     #endregion
 
+    #region SlicingChecking
+    [Header("Slicing Checking")]
+    [SerializeField] private Vector3 _lrDirection;
+    #endregion
+
     private void Awake()
     {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
         _cam = Camera.main;
     }
 
@@ -44,8 +64,16 @@ public class BladeTime : MonoBehaviour
             if (_initialPoint == Vector3.zero) return;
             if (_finalPoint == Vector3.zero) return;
 
+            // sets plane rotation
             _cutPos = new Vector3(_finalPoint.x - _initialPoint.x, _finalPoint.y - _initialPoint.y, 0);
+            _cutPos.Normalize();
+
             RotateCuttingPlane(_cutPos);
+
+            // checks if rotation is similar to objective
+            if (Mathf.Abs(_cutPos.x - _lrDirection.x) >= 0.1) return;
+            if (Mathf.Abs(_cutPos.y - _lrDirection.y) >= 0.1) return;
+
             Slice();
         }
     }
@@ -71,6 +99,8 @@ public class BladeTime : MonoBehaviour
                 Destroy(hits[i].gameObject);
             }
         }
+
+        Instantiate(_cubePrefab, _cubeSpawner.position, Quaternion.identity);
     }
 
     private void AddHullComponents(GameObject go)
@@ -99,5 +129,26 @@ public class BladeTime : MonoBehaviour
         _cuttingPlane.rotation = rotation;
         _initialPoint = Vector3.zero;
         _finalPoint = Vector3.zero;
+    }
+    // lo hice singleton para facilitarme la vida
+    private static BladeTime _instance;
+
+    private void OnDestroy()
+    {
+        if (_instance != this)
+        {
+            _instance = this;
+        }
+    }
+
+    public static BladeTime GetInstance
+    {
+        get { return _instance; }
+    }
+
+    public Vector3 SetLrDirection
+    {
+        get { return _lrDirection; }
+        set { _lrDirection = value; }
     }
 }
