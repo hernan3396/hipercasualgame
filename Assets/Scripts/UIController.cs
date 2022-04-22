f1e6cb4de38fb201fd65045d04e3265bb5698a21
hernan3396 <hernan3396@gmail.com>
2022-04-21 21:31:31 -0300

    added ui controller

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIController : MonoBehaviour
+{
+
+    [Header("Score")]
+    [SerializeField] private int _pScore;
+    [SerializeField] private TextMeshProUGUI _scoreText;
+
+
+    [Header("Life")]
+    [SerializeField] private int _pLife;
+    [SerializeField] private int _MaxLife;
+    [SerializeField] private GameObject[] _ImagenVida;
+
+    [Header("GameOver")]
+    [SerializeField] private GameObject _panelGameOver;
+    [SerializeField] private TextMeshProUGUI _scoreGOText;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _scoreText.text = "Score: " + _pScore;
+        if (_pLife > 2)
+        {
+            _ImagenVida[0].SetActive(true);
+            _ImagenVida[1].SetActive(true);
+            _ImagenVida[2].SetActive(true);
+        }
+        else if (_pLife > 1)
+        {
+            _ImagenVida[0].SetActive(true);
+            _ImagenVida[1].SetActive(true);
+            _ImagenVida[2].SetActive(false);
+        }
+        else if (_pLife > 0)
+        {
+            _ImagenVida[0].SetActive(true);
+            _ImagenVida[1].SetActive(false);
+            _ImagenVida[2].SetActive(false);
+        }
+        else
+        {
+            /*
+            _panelGameOver.SetActive(true);
+            _scoreGOText.text = "Tu puntaje es de: " + _pScore;
+            */
+        }
+
+    }
+
+    public void AddScore(int addScore)
+    {
+        _pScore += addScore;
+    }
+}
