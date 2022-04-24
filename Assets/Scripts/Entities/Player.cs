d954fe1a082afbe24378c9215fee6e9519ea8d67
hernan3396 <hernan3396@gmail.com>
2022-04-24 13:51:04 -0300

    changes on description :(
    is affected by game start/pause/over
    moved parameters to a scriptable object
    added death and start death methods

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -1,19 +1,95 @@
+using System;
 using UnityEngine;
-
+[RequireComponent(typeof(Animator))]
 public class Player : Entity
 {
-    public void TestCol()
+    #region Components
+    private UIController _uiController;
+    private Animator _animator;
+    #endregion
+
+    [SerializeField] private protected PlayerData _playerData;
+    private bool _gamePaused = false;
+
+    #region Parameters
+    protected float _deathTime;
+    protected bool _isGameOver;
+    #endregion
+
+    private void Awake()
     {
-        Debug.Log("Choco");
+        _inmunityTime = _playerData.InmunityTime;
+        _deathTime = _playerData.DeathTime;
+        _maxHealth = _playerData.MaxHealth;
+        _currentHealth = _maxHealth;
+
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        _uiController = GameManager.GetInstance.GetUIController;
+
+        GameManager.GetInstance.onGameOver += OnGameOver;
+        GameManager.GetInstance.onGamePause += OnGamePause;
+    }
+
+    public override void TakeDamage(int value, int knockback)
+    {
+        if (_isGameOver) return;
+        if (_isInmune) return; // esta en base, pero por lo visto es necesario aca tambien (?)
+        base.TakeDamage(value, knockback);
+        _uiController.UpdateLifes(_currentHealth);
+        _animator.SetBool("isDamaged", true);
     }
 
     protected override void StartDeath()
     {
-        throw new System.NotImplementedException();
+        if (_isGameOver) return;
+        _animator.SetBool("startDeath", true);
     }
 
     public override void Death()
     {
-        throw new System.NotImplementedException();
+        // aca podrias usar la animacion de muerte
+        if (_isGameOver) return;
+        GameManager.GetInstance.GameOver();
+    }
+
+    private void OnGamePause(bool value)
+    {
+        _gamePaused = value;
+    }
+
+    private void OnGameOver(bool value)
+    {
+        _isGameOver = value;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.GetInstance.onGamePause -= OnGamePause;
+        GameManager.GetInstance.onGameOver -= OnGameOver;
+    }
+
+    public float GetInmunityTime
+    {
+        get { return _inmunityTime; }
+    }
+
+    public float GetDeathTime
+    {
+        get { return _deathTime; }
+    }
+
+    public bool IsInmune
+    {
+        get { return _isInmune; }
+        set { _isInmune = value; }
+    }
+
+    public bool IsPlayerPaused
+    {
+        get { return _gamePaused; }
     }
 }
